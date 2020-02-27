6948c2fbac7789c8332c626a7b20991767fcd835
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-02-27 17:52:17 +0900

    VueCliのデフォルトの表示の削除

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
@@ -22,12 +15,9 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data () {
     return {
