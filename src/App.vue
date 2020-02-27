25df7db1a73b80e74c859953dc183e99c309fb99
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-02-27 17:44:49 +0900

    vuetifyのダウングレード

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-
       <v-btn
+        flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
-        text
       >
         <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-toolbar>
 
     <v-content>
       <HelloWorld/>
@@ -44,17 +22,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    HelloWorld
   },
-
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      //
+    }
+  }
+}
 </script>
