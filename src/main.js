4c0ce661efc70cba65500a6443d1b69a6b58d506
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-03-01 22:56:12 +0900

    firebaase authenticationの設定

--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGWbc2c9G3xLtn4-ztefAndYGsc47hK1A",
+  authDomain: "vue-address-application-server.firebaseapp.com",
+  databaseURL: "https://vue-address-application-server.firebaseio.com",
+  projectId: "vue-address-application-server",
+  storageBucket: "vue-address-application-server.appspot.com",
+  messagingSenderId: "188515208054",
+  appId: "1:188515208054:web:d05b0baef97f335d15c857",
+  measurementId: "G-TGT7QNLBNP"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
